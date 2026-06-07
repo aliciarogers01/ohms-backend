015bde698fe0fcb306d2ca7c5c0880dd10a01756
aliciarogers01 <aleigh0435@gmail.com>
2026-06-06 23:38:37 -0400

    Add OHMS front page

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+
 app.get("/admin", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "admin.html"));
 });
